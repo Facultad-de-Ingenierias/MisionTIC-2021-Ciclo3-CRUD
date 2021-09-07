cc9c8cc84befeb0e0b1d2f41a41d46923bd9d00f
Profe Oscar <oscar.franco@ucaldas.edu.co>
2021-09-07 13:24:27 +0000

    Initial Commit

---
+++ HolaWeb.App.Persistencia/AppRepositorios/IRepositorioSaludos.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using HolaWeb.App.Dominio;
+
+namespace HolaWeb.App.Persistencia.AppRepositorios
+{
+    public interface IRepositorioSaludos
+    {
+        IEnumerable<Saludo> GetAll();
+         
+    }
+}
